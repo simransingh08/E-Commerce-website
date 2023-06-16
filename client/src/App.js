dee9ecdde6ea67a37dca76a755ecec2eb7b8599c
simransin <simransingh9876rajgir@gmail.com>
2023-06-16 23:19:14 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="//:id" element={<Category />} />
+                    <Route path="/category/:id" element={<Category />} />
                     <Route path="/product/:id" element={<SingleProduct />} />
                 </Routes>
                 <Newsletter />
